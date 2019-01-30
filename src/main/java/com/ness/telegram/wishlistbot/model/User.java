dc3ae6d2266116f666310a3def3de0dea229243f
Roman <bezruchkor1@gmail.com>
2019-01-30 22:26:42 +0200

    added jpa model bidirectional associations

--- src/main/java/com/ness/telegram/wishlistbot/model/User.java
+++ src/main/java/com/ness/telegram/wishlistbot/model/User.java
@@ -1,23 +1,32 @@
 package com.ness.telegram.wishlistbot.model;
 
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import lombok.*;
 
 @NoArgsConstructor
 @Getter 
 @Setter
-@ToString
-@EqualsAndHashCode
+@ToString(exclude = "wishes")
+@EqualsAndHashCode(exclude = "wishes")
 @Entity
 @Table(name = "uzer")
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_generator")
+    @SequenceGenerator(name = "user_generator", sequenceName = "user_seq", allocationSize = 10)
     private Long id;
 
     @Column(name = "telegram_chat_id")
@@ -26,4 +35,7 @@ public class User {
     @Column(name = "state")
     @Enumerated(EnumType.STRING)
     private State state = State.DEFAULT;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Wish> wishes;
 }
--- src/main/java/com/ness/telegram/wishlistbot/model/Wish.java
+++ src/main/java/com/ness/telegram/wishlistbot/model/Wish.java
@@ -2,20 +2,28 @@ package com.ness.telegram.wishlistbot.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import lombok.*;
 
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
-@EqualsAndHashCode
+@ToString(exclude = "user")
+@EqualsAndHashCode(exclude = "user")
 @Entity
 @Table(name = "wish")
 public class Wish {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "wish_generator")
+    @SequenceGenerator(name = "wish_generator", sequenceName = "wish_seq", allocationSize = 50)
     private Long id;
 
     @Column(name = "label")
@@ -23,4 +31,8 @@ public class Wish {
 
     @Column(name = "link")
     private String link;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn
+    private User user;
 }
