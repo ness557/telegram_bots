1decdf7b22832d4f82262d837addb80eb1b84983
Roman <bezruchkor1@gmail.com>
2019-01-30 22:40:14 +0200

    added logging to services

--- src/main/java/com/ness/telegram/wishlistbot/service/UserServiceImpl.java
+++ src/main/java/com/ness/telegram/wishlistbot/service/UserServiceImpl.java
@@ -4,8 +4,10 @@ import com.ness.telegram.wishlistbot.model.User;
 import com.ness.telegram.wishlistbot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -13,21 +15,31 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save(User user) {
+        log.info("Saving user [{}]", user);
         repository.save(user);
+        log.info("Saved");
     }
 
     @Override
     public void delete(User user) {
+        log.info("Deleting user [{}]", user);
         repository.delete(user);
+        log.info("Deleted");
     }
 
     @Override
     public User get(Long id) {
-        return repository.getOne(id);
+        log.info("Getting user by id [{}]", id);
+        User user = repository.getOne(id);
+        log.info("Got user [{}]", user);
+        return user;
     }
 
     @Override
     public User findByChatId(Integer chatId) {
-        return repository.findByChatId(chatId);
+        log.info("Finding user by chat id [{}]", chatId);
+        User user = repository.findByChatId(chatId);
+        log.info("Found user [{}]", user);
+        return user;
     }
 }
--- src/main/java/com/ness/telegram/wishlistbot/service/WishServiceImpl.java
+++ src/main/java/com/ness/telegram/wishlistbot/service/WishServiceImpl.java
@@ -5,8 +5,10 @@ import com.ness.telegram.wishlistbot.model.Wish;
 import com.ness.telegram.wishlistbot.repository.WishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class WishServiceImpl implements WishService {
 
     @Autowired
@@ -14,26 +16,38 @@ public class WishServiceImpl implements WishService {
 
     @Override
     public void save(Wish wish) {
+        log.info("Saving wish [{}]", wish);
         repository.save(wish);
+        log.info("Saved");
     }
 
     @Override
     public void delete(Wish wish) {
+        log.info("Deleting wish [{}]", wish);
         repository.delete(wish);
+        log.info("Deleted");
     }
 
     @Override
     public void delete(Long id) {
+        log.info("Deleting wish by id [{}]", id);
         repository.deleteById(id);
+        log.info("Deleted");
     }
 
     @Override
     public Wish get(Long id) {
-        return repository.getOne(id);
+        log.info("Getting wish by id [{}]", id);
+        Wish wish = repository.getOne(id);
+        log.info("Got wish [{}]", wish);
+        return wish;
     }
 
     @Override
     public Wish findByLabelAndUser(String label, User user) {
-        return findByLabelAndUser(label, user);
+        log.info("Finding wish by label [{}] and user [{}]", label, user);
+        Wish wish = repository.findByLabelAndUser(label, user);
+        log.info("Found user [{}]", wish);
+        return wish;
     } 
 }
